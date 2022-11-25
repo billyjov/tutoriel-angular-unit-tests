4cb8e73dc5e651a0b7852613ecd005538b21d855
Billy Lando <billy_lando@yahoo.com>
2022-11-25 11:53:26 +0100

    feat: add part-7

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -11,30 +14,22 @@ describe('AppComponent', () => {
       imports: [ReactiveFormsModule]
     });
 
-    console.log('HELLO');
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
 
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'unit-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('unit-test');
+    expect(app.title).toBe('unit-test');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('unit-test app is running!');
-  });
-
-  it('should', () => {
-    expect(true).toBe(true);
   });
 });
